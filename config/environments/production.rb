bf49f8fba7f95b446e1f8d506b014110e8146fac
A. Speller <a.speller@digital-science.com>
2013-08-29 21:13:04 +0200

    Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Emberwang::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.ember.variant = :production
+
 end
