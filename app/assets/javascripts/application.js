992f70f5bc6b9505489266c2d6c0bf88b1b17ff9
A. Speller <a.speller@digital-science.com>
2013-08-29 09:38:31 +0200

    Initialize

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require emberwang
+
+// for more details see: http://emberjs.com/guides/application/
+Emberwang = Ember.Application.create();
+
 //= require_tree .
---
+++ app/assets/javascripts/router.js.coffee
@@ -0,0 +1,5 @@
+# For more information see: http://emberjs.com/guides/routing/
+
+Emberwang.Router.map ()->
+  # @resource('posts')
+
