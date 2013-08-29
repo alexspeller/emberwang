540e3a9c8f4d0e4b17165cdcd790fa276e6d51d1
A. Speller <a.speller@digital-science.com>
2013-08-29 09:37:48 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emberwang::Application.config.secret_key_base = '16b1576a87c95af832593548b6f579f092abcff82a6303f5360c5014e5e9d3152d33be5369175e2c92f2e7e3e4a4c9594095ccc49b1ad5b5dfea7611e72c3b95'
