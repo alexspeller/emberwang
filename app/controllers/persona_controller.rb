5245cbd7dde3b9b70202a74eb58c116978a38c06
A. Speller <a.speller@digital-science.com>
2013-08-30 23:31:02 +0200

    Fix persona

--- app/controllers/persona_controller.rb
+++ app/controllers/persona_controller.rb
@@ -4,7 +4,11 @@ class PersonaController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def login
     url = URI "https://verifier.login.persona.org/verify"
-    response = Net::HTTP.post_form url, assertion: params[:assertion], audience: "http://localhost:3000"
+
+    audience = "#{request.protocol}#{request.host}:#{request.port}"
+
+    response = Net::HTTP.post_form url, assertion: params[:assertion], audience: audience
+
     if response.code == "200"
       body = JSON.parse response.body
       session[:email] = body["email"]
@@ -17,7 +21,7 @@ class PersonaController < ApplicationController
   end
 
   def logout
-    reset_session!
+    reset_session
     head :ok
   end
 end
